bb5cbf38c5d41964526617d10cb3d79ac52e5b6e
Jhon Jairo <jhonsagar9804@gmail.com>
2022-06-18 02:00:43 -0500

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/semana1_ejercicios/Semana1_Ejercicios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.semana1_ejercicios;
+
+/**
+ *
+ * @author jhon sanchez
+ */
+public class Semana1_Ejercicios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
